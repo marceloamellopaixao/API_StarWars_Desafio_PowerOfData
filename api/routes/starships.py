104abce3ae8c5e7e3dfe65ec64651f2ca40a7411
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-11-08 18:52:16 -0300

    feat: Adicionado o metodo de obter as estatisticas das naves estelares

--- api/routes/starships.py
+++ api/routes/starships.py
@@ -82,3 +82,59 @@ def get_starships():
         return jsonify(response)
     except Exception as e:
         return jsonify({'Erro reconhecido': str(e)}), 404
+
+
+@starships.route('/starships/statistics', methods=['GET'])
+@jwt_required()
+def get_starships_statistics():
+    """Obtém estatísticas de naves estelares
+    ---
+    tags:
+      - Naves Estelares
+    description: Retorna estatísticas sobre as naves estelares disponíveis na API, incluindo contagem total, distribuição de fabricantes e classes de naves estelares..
+    responses:
+      200:
+        description: Estatísticas das naves estelares.
+        schema:
+          type: object
+          properties:
+            Total de Naves Estelares:
+              type: integer
+              description: Total de naves estelares disponíveis.
+            Distribuição de Fabricantes:
+              type: object
+              additionalProperties:
+                type: integer
+                description: Contagem de naves estelares por Fabricantes.
+            Distribuição de Classes de Naves Estelares:
+              type: object
+              additionalProperties:
+                type: integer
+                description: Contagem de naves estelares por Classes de Naves Estelares.
+      404:
+        description: Nenhum dado encontrado ou erro durante a busca.
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              description: Mensagem de erro retornada.
+    """
+    starships = search_data('starships/')
+    starships_list = starships.get('results', [])
+    total_starships = len(starships_list)
+
+    try:
+        contabilizar_atributos = ['manufacturer', 'starship_class']
+        response = get_statistics_func(starships_list, contabilizar_atributos)
+
+        contabilizador_manufacturer = response['manufacturer']
+        contabilizador_starship_class = response['starship_class']
+
+        return jsonify({
+            'Total de Naves Estelares': total_starships,
+            'Distribuicao de Fabricantes': contabilizador_manufacturer,
+            'Distribuicao de Classes de Naves Estelares': contabilizador_starship_class
+        })
+    except Exception as e:
+        return jsonify({'Erro reconhecido': str(e)}), 404

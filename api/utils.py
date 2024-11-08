3406f24fdfc062448d17c3c730777841559ec20b
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-11-08 18:32:40 -0300

    feat: Adicionado a Função de Filtragem dos Dados

--- api/utils.py
+++ api/utils.py
@@ -11,3 +11,26 @@ def search_data(endpoint):
             return {'Erro reconhecido': 'Dado não encontrado'}, response.status_code
     except Exception as e:
         return {'Erro reconhecido': str(e)}, response.status_code
+
+
+# Função de Filtragem dos Dados
+def filter_data(data_list, **search_params):
+    """Filtra a lista de dados de acordo com os critérios de pesquisa.
+
+    Args:
+        data_list (list): A lista de dados a ser filtrada.
+        search_params (dict): Parâmetros de pesquisa.
+
+    Returns:
+        list: A lista filtrada.
+    """
+    filtered_data = []
+    for item in data_list:
+        match = True
+        for key, value in search_params.items():
+            if value and value.lower() not in str(item.get(key, '')).lower():
+                match = False
+                break
+        if match:
+            filtered_data.append(item)
+    return filtered_data
